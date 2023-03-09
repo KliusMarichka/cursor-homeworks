41901db0b74bafa81f76625132a7f2c44b450b1c
KliusMarichka <125118927+KliusMarichka@users.noreply.github.com>
2023-03-09 19:46:59 +0200

    HW2 work is still progressing

--- HW2/index.js
+++ HW2/index.js
@@ -1,25 +1,46 @@
-let n = prompt('Будь ласка, введіть число');
+let number1 = +prompt("Ціле число, будь ласка");
+let N = Number.isInteger(number1) ? number1 : document.writeln('Try again');
+console.log(N);
 
-if (Number.isInteger(n)) {
-    console.log('Число ціле');
-} else if (Number.isNaN(n)) {
-    console.log('Це не число');
-} else {
-    console.log('Число неціле');
+let number2 = +prompt("Ще одне ціле число, будь ласка");
+let M = Number.isInteger(number2) ? number2 : document.writeln('Try again');
+console.log(M);
+
+let skipEven = confirm('Пропускати парні числа?');
+
+let sum = 0;
+
+while (N < M) {
+    if (skipEven == true && (N % 2) == 0) {
+        sum = sum + N;
+        N = N + 2;
+    } else {
+        sum = sum + N;
+        N = N + 1;
+    }
 }
 
-let m = +prompt('Будь ласка, введіть ще одне число');
-
-if (Number.isInteger(m)) {
-    console.log('Число ціле');
-} else if (Number.isNaN(m)) {
-    console.log('Це не число');
+if (skipEven == false && (N % 2) == 0) {
+console.log(sum+M);
+} else if ((M % 2) == 0) {
+console.log(sum)
 } else {
-    console.log('Число неціле');
+console.log(sum+M);
 }
 
-let areNumbersEven = confirm('Пропускати парні числа?');
 
-//for (let ) {
-//console.log()
-//}
+/*for (let j = (N + 1); j < (M - N) + 1; j++) {
+console.log(N + j + M);
+}
+
+//while (i > m) {
+    i = i++
+if (skipEven == true, i % 2 != 0)  { 
+    console.log(i);
+    i = i + (i + 2);
+} else {
+    console.log(i+1)
+    i = i + 2
+}
+}
+*/
