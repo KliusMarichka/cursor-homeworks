63514215a38955bc1fb7723194931c357f0b7bb9
KliusMarichka <125118927+KliusMarichka@users.noreply.github.com>
2023-03-15 20:19:31 +0200

    changed console.log into return in the functions

--- HW3/index.js
+++ HW3/index.js
@@ -1,17 +1,3 @@
-
-// Задача 2
-function getPow(number, power) {
-    let numberPow;
-    numberPow = number * number;
-    for (let i=1; i<=power-2; i++) {
-        numberPow = numberPow * number;
-    }  
-    console.log(numberPow);
-}
-
-getPow(3, 9);
-
-
 //Задача 1
 
 function getMaxDigit(number) {
@@ -33,32 +19,44 @@ function getMaxDigit(number) {
            maxNum = num2;
        }
     }
-    console.log(maxNum);
+    return maxNum;
 }
-getMaxDigit(1092);
+console.log(getMaxDigit(1092));
    
+// Задача 2
+function getPow(number, power) {
+    let numberPow;
+    numberPow = number * number;
+    for (let i=1; i<=power-2; i++) {
+        numberPow = numberPow * number;
+    }  
+    return numberPow;
+}
+
+console.log(getPow(3, 4));
+
 // Задача 3
 function startCapital(name) {
    const letters = name.split('');
    const cut = name.substring(1);
-   console.log(`${letters[0].toUpperCase()}${cut.toLowerCase()}`); 
+   return `${letters[0].toUpperCase()}${cut.toLowerCase()}`; 
 }
 
-startCapital('валЄрА');
+console.log(startCapital('валЄрА'));
 
-//Задача 4
+// Задача 4
 let grossPay;
 let netPay;
 
 function countNetSalary(grossPay) {
     tax = ((grossPay*18)/100)+((grossPay*1.5)/100);
     netPay = grossPay - tax;
-    console.log(netPay);
+    return netPay;
 }
 
-countNetSalary(10000);
+console.log(countNetSalary(10000));
 
-//Задача 5
+// Задача 5
 
 /*function getRandomNumber (num1, num2) {
     const random = Math.random(num1, num2)
@@ -68,11 +66,11 @@ getRandomNumber(0.02, 0.9);
 */
 
 function getRandomInteger(n, m) {
-    console.log(Math.floor(Math.random() * (m - n) + n));
+    return Math.floor(Math.random() * (m - n) + n);
 }
-getRandomInteger(3, 10);
+console.log(getRandomInteger(3, 10));
 
-//Задача 6
+// Задача 6
 
 let result = 0;
 let word;
@@ -84,8 +82,7 @@ function countLetters (word, L) {
         result++
         position = word.indexOf(L, position + 1);
     }
-    console.log(result);
+    return result;
 }
-
-countLetters("абабагаламага", "а");
+console.log(countLetters("абабагаламага", "а"));
 
