757fc30418b367d5d72ecba1d9a070ad3d61a01d
KliusMarichka <125118927+KliusMarichka@users.noreply.github.com>
2023-03-16 13:48:56 +0200

    did tasks 2 and 3. not sure about the 4th though

--- HW4/index.js
+++ HW4/index.js
@@ -7,7 +7,7 @@ const students = [
 'Світлана'
 ];
 
-const themes = [
+const topics = [
 'Диференційне рівняння',
 'Теорія автоматів',
 'Алгоритми і структури даних'
@@ -21,13 +21,11 @@ const marks = [
 4,
 5
 ];
+
 // Завдання 1
 const girls = [];
 const boys = [];
-const pair1 = [];
-const pair2 = [];
-const pair3 = [];
-const pair = [[], [], []];
+const pairArr = [[], [], []];
 
 function getPairs() {
     for (let i = 0; i < students.length; i++) {
@@ -41,13 +39,53 @@ function getPairs() {
     }
 
     for (let i = 0; i < boys.length; i++) {
-    pair[i].push(boys[i]);
-    pair[i].push(girls[i]);
+    pairArr[i].push(boys[i]);
+    pairArr[i].push(girls[i]);
     }
-    return pair
+    return pairArr
 }
 
 const pairs = getPairs();
 console.log(pairs);
 
 // Завдання 2
+
+function givePairsTopics (){
+    for (let i = 0; i < topics.length; i++) {
+        let topic = topics[i];
+        pairArr[i].push(topic);
+    }
+    return pairArr
+}
+
+givePairsTopics();
+
+// Завдання 3
+const studMarks = [[], [], [], [], [], []];
+
+function giveStudentsMarks (){
+    for (let i = 0; i < marks.length; i++) {
+        let name = students[i];
+        let mark = marks[i];
+        studMarks[i].push(name);
+        studMarks[i].push(mark);
+    }
+    return studMarks
+}
+
+const markedStudents = giveStudentsMarks();
+console.log(markedStudents);
+
+// Задача 4
+let pairsGraded = []; 
+function gradePairs (){
+    for (let i = 0; i < 3; i++) {
+        let gradedPair = pairArr[i];
+        let grade = Math.floor(Math.random() * (6 - 1) + 1);
+        pairsGraded.push(gradedPair);
+        pairsGraded.push(grade);
+    }
+    return pairsGraded
+}
+
+console.log(gradePairs());
