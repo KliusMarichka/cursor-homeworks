69d6cb777c2f5eb8d1590e39db77ca14691019c9
KliusMarichka <125118927+KliusMarichka@users.noreply.github.com>
2023-03-26 08:37:24 +0300

    3/6 done

--- HW5/index.js
+++ HW5/index.js
@@ -12,10 +12,33 @@ console.log(getRandomArray(15, 1, 100));
 
 // Задача 2
 
+/*const numRow =[];
+function getModa (...numbers) {
+  numbers.filter ((number) => {
+    if (number % parseInt(number) === 0) {
+      numRow.push(number)
+    }
+    return numRow
+  })    
+  const sorted = numRow.sort((a, b) => a - b)
+  return sorted
+  let num1;
+  let num2 = 0;
+  let num3;
+    for (i = 0; i<numRow.length; i++) {
+      num1 = numRow[i];
+      if (num1 = numRow[i+1]) {
+      num2 = num2 + 1;
+      } else {
 
+      }
+  }
+}
+console.log(getModa(2, 34, 2, 56.1, 3, 2, 3.1, 78));
+*/
 
 // Задача 3
-
+let count;
 const numNums =[];
 function getAverage (...numbers) {
   numbers.filter ((number) => {
@@ -32,3 +55,14 @@ function getAverage (...numbers) {
   }
   
   console.log(getAverage(6, 2, 3.1, 2, 2.1, 2));
+
+  // Задача 5
+  function filterEvenNumbers(...numbers) {
+    const evenNums = numbers.filter((number) => {
+      return number % 2 === 0
+    })
+    return evenNums
+  }
+
+  console.log(filterEvenNumbers(1, 2, 5, 6, 3, 8, 10, 12));
+
